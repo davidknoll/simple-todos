d077b735b1f09796559a824193e3bd458f76b4e7
David Knoll <david@futurefirst.org.uk>
2016-04-28 13:01:59 +0100

    add some extra tests for deleting public vs private tasks

--- imports/api/tasks.test.js
+++ imports/api/tasks.test.js
@@ -11,7 +11,7 @@ if (Meteor.isServer) {
     describe('methods', () => {
       const userId = Random.id();
       let taskId;
-      
+
       beforeEach(() => {
         Tasks.remove({});
         taskId = Tasks.insert({
@@ -22,21 +22,64 @@ if (Meteor.isServer) {
         });
       });
 
-      it('can delete owned task', () => {
+      it('can delete owned public task', () => {
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
-        
+
         // Set up a fake method invocation that looks like what the method expects
         const invocation = {
           userId
         };
-        
+
         // Run the method with `this` set to the fake invocation
         deleteTask.apply(invocation, [taskId]);
-        
+
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('can delete owned private task', () => {
+        // Make the test task private
+        const privTask = Meteor.server.method_handlers['tasks.setPrivate'];
+        privTask.apply({userId}, [taskId, true]);
+
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = {
+          userId
+        };
+
+        // Run the method with `this` set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
+      });
+
+      it('cannot delete unowned private task', () => {
+        // Make the test task private
+        const privTask = Meteor.server.method_handlers['tasks.setPrivate'];
+        privTask.apply({userId}, [taskId, true]);
+
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        // Set up a fake method invocation that looks like what the method expects
+        const newUserId = Random.id(); // Become another user
+        const invocation = {
+          newUserId
+        };
+
+        // Run the method with `this` set to the fake invocation
+        assert.throws(
+          () => deleteTask.apply(invocation, [taskId]),
+          Meteor.Error, 'not-authorized'
+        );
       });
     });
   });
